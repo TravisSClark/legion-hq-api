3e342ae5f659b57a0cc91130e7714b28db64354c
Travis Clark <travis.scott.clark@gmail.com>
2024-09-18 21:02:50 -0400

    Changing the routes

--- routes/user_list.routes.js
+++ routes/user_list.routes.js
@@ -1,8 +1,8 @@
 import user_lists from '../controllers/user_list.controller';
 
 exports = (app) => {
-  app.get('/lists', user_lists.findSome);
-  app.get('/lists/:listId', user_lists.findOne);
+  app.get('/lists', user_lists.findListsForUser);
+  app.get('/lists/:listId', user_lists.findList);
   app.post('/lists', user_lists.create);
   app.put('/lists/:listId', user_lists.update);
   app.delete('/lists/:listId', user_lists.delete);
